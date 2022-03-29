887ec986834c9875ab8cf4cd675ab5e3cffa977b
Keira Dueck <sylae@calref.net>
2022-03-29 05:13:22 -0700

    threw together a quick landing page

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WormRP</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link crossorigin="anonymous" media="all" rel="stylesheet" href="/base.css" />
+</head>
+
+<body>
+	<nav class="navbar">
+		<div class="noChonk">
+			<span class="site-branding">
+				<a href="/">WormRP</a>
+			</span>
+			<ul class="nav-items-standard">
+				<li><a href="https://www.reddit.com/r/wormrp">Reddit</a></li>
+				<li><a href="https://discord.gg/KjYAwes">Discord</a></li>
+				<li><a href="https://wiki.wormrp.com">Wiki</a></li>
+				<!-- <li><a href="/map.php">Map</a></li> -->
+				<!-- <li><a href="/reports/">Reports</a></li> -->
+			</ul>
+		</div>
+	</nav>
+	<main>
+	<h1>About Us</h1>
+		<p>Welcome to  WormRP! We are a roleplay community set in the setting of <a href="https://parahumans.wordpress.com/"><em>Worm</em></a>, a superhero web serial made by Wildbow. Our setting is an Alternative Universe of that found in canon, and while reading the novel is encouraged, it is in no way required to hop into the action.</p>
+		<p>Based in the city of Devilfish, Minnesota, a city that has just crossed the threshold between medium-sized boom-town into a flourishing major city, and with that increase of wealth, size, and population comes the inevitable flood of super powered parahumans looking to capitalize on it.</p>
+		<p>Be they hero, villain, and all those in-between, we invite you to join us and help us build this setting together!</p>
+	
+	
+	<hr />
+	
+	</main>
+	
+	<footer>
+		<p><small>Copyright © 2015-<?php echo date('Y'); ?> /r/WormRP contributors.<br />Content is available under <a rel="nofollow" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public License</a> unless otherwise noted.</small></p>
+	</footer>
+	
+</body>
+
+</html>
