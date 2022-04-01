df6aa84e1d4ab3e54b67f3169a5c58dc9cbe3cbf
Keira Dueck <sylae@calref.net>
2022-03-31 21:30:09 -0700

    sort

--- src/QueueItem.php
+++ src/QueueItem.php
@@ -28,7 +28,7 @@ class QueueItem
     public static function getQueue(): Collection
     {
         global $db; // i know
-        $query = $db->executeQuery("select * from wormrp_queue");
+        $query = $db->executeQuery("select * from wormrp_queue order by postTime ASC");
         $c = new CharlotteDunois\Collect\Collection();
         while ($res = $query->fetchAssociative()) {
             $x = self::createFromDBrow($res);
