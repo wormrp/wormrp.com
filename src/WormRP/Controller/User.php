1a9f12611ca380593286f8c9df5342d084fedace
Keira Dueck <sylae@calref.net>
2023-07-03 11:09:32 -0700

    ughhhhhhhhhhhhhhhhhhhhhhhhhh

--- src/WormRP/Controller/User.php
+++ src/WormRP/Controller/User.php
@@ -54,52 +54,48 @@ class User extends Controller
             'code' => $code
         ]);
 
-        try {
-            // object(Wohali\OAuth2\Client\Provider\DiscordResourceOwner)[36]
-            //  protected 'response' =>
-            //    array (size=14)
-            //      'id' => string '297969955356540929' (length=18)
-            //      'username' => string 'misfitmaid' (length=10)
-            //      'global_name' => string 'MisfitMaid' (length=10)
-            //      'avatar' => string 'e0f981cd0358e3adf5c4702dac7cdd75' (length=32)
-            //      'discriminator' => string '0' (length=1)
-            //      'public_flags' => int 768
-            //      'flags' => int 768
-            //      'banner' => string 'a_0ef8cbb956f37768e74929dcb1e1f36f' (length=34)
-            //      'banner_color' => null
-            //      'accent_color' => null
-            //      'locale' => string 'en-GB' (length=5)
-            //      'mfa_enabled' => boolean true
-            //      'premium_type' => int 2
-            //      'avatar_decoration' => null
+        // object(Wohali\OAuth2\Client\Provider\DiscordResourceOwner)[36]
+        //  protected 'response' =>
+        //    array (size=14)
+        //      'id' => string '297969955356540929' (length=18)
+        //      'username' => string 'misfitmaid' (length=10)
+        //      'global_name' => string 'MisfitMaid' (length=10)
+        //      'avatar' => string 'e0f981cd0358e3adf5c4702dac7cdd75' (length=32)
+        //      'discriminator' => string '0' (length=1)
+        //      'public_flags' => int 768
+        //      'flags' => int 768
+        //      'banner' => string 'a_0ef8cbb956f37768e74929dcb1e1f36f' (length=34)
+        //      'banner_color' => null
+        //      'accent_color' => null
+        //      'locale' => string 'en-GB' (length=5)
+        //      'mfa_enabled' => boolean true
+        //      'premium_type' => int 2
+        //      'avatar_decoration' => null
 
-            $resp = $this->oauth->provider->getResourceOwner($token)->toArray();
+        $resp = $this->oauth->provider->getResourceOwner($token)->toArray();
 
-            $user = \WormRP\Model\User::findByAttributes(array('idUser' => $resp['id']));
+        $user = \WormRP\Model\User::findByAttributes(array('idUser' => $resp['id']));
 
-            if (!$user) {
-                $user = new User();
-                $user->idUser = (int)$resp['id'];
-                $user->username = $resp['username'];
-                $user->displayName = $resp['global_name'];
-            }
-            $user->avatar = $resp['avatar'];
-            $user->banner = $resp['banner'];
-            $user->save();
-
-
-            Nin::setuid($user->idUser);
-            Nin::setSession('csrf_token', Nin::randomString(32));
-
-            $redirect = Nin::getSession("postAuthRedirect");
-            if ($redirect) {
-                $this->redirect($redirect);
-            } else {
-                $this->redirect("/");
-            }
-        } catch (\Throwable) {
-            $this->displayError("Unknown error. Please try again or seek help.", 500);
-            return;
+        if (!$user) {
+            $user = new User();
+            $user->idUser = (int)$resp['id'];
+            $user->username = $resp['username'];
+            $user->displayName = $resp['global_name'];
         }
+        $user->avatar = $resp['avatar'];
+        $user->banner = $resp['banner'];
+        $user->save();
+
+
+        Nin::setuid($user->idUser);
+        Nin::setSession('csrf_token', Nin::randomString(32));
+
+        $redirect = Nin::getSession("postAuthRedirect");
+        if ($redirect) {
+            $this->redirect($redirect);
+        } else {
+            $this->redirect("/");
+        }
+
     }
 }
