67e7be4f9a84cd3223001286cd3baf668ab69ef9
Keira Dueck <sylae@calref.net>
2023-11-27 06:53:49 -0800

    remove thread creation per nashoid

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ namespace WormRP {
 
     nf_route("/threads", "WormRP\Controller\Threads.Index");
     nf_route("/threads/search", "WormRP\Controller\Threads.Search");
-    nf_route("/threads/new", "WormRP\Controller\Threads.New");
+    // nf_route("/threads/new", "WormRP\Controller\Threads.New");
 
     nf_route("/thread/:idThread", "WormRP\Controller\Thread.View");
     nf_route("/thread/:idThread/edit", "WormRP\Controller\Thread.Edit");
